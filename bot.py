c0b7af945100586547363f37218977138ea9b0d6
Akshay Jain <akshayjain3923@hotmail.com>
2026-04-13 16:55:08 -0400

    Update the feed link

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ import feedparser
 import requests
 
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL", "")
-RSS_FEED_URL = "https://intel.taggartinstitute.org/i/?a=rss"
+RSS_FEED_URL = "https://news.ifin.network/i/?a=rss"
 SEEN_FILE = Path(__file__).parent / "seen_articles.json"
 
 
